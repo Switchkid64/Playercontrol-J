4d14470d8d2b7530d7eac7408bad9b60566d084b
Switchkid64 <minaj@roverkids.org>
2025-04-10 13:48:14 -0400

    1.2 completed

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
+
+//nothing here yet...
