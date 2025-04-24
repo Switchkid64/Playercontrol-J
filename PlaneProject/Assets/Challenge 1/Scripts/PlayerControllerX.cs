9d24f453d762a7ba221f8845ff737861c0d3a6b0
Switchkid64 <minaj@roverkids.org>
2025-04-24 13:47:50 -0400

    and this too

--- PlaneProject/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ PlaneProject/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -7,6 +7,7 @@ public class PlayerControllerX : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
+    public float spinPropellerZ
 
     // Start is called before the first frame update
     void Start()
@@ -25,5 +26,8 @@ public class PlayerControllerX : MonoBehaviour
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+
+        //propellers will propel
+        transform.Rotate()
     }
 }
